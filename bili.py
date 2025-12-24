c851543e1fe4809679fa9bd666a65cbf0e66a01e
EveryEunbin <116586151+EveryEunbin@users.noreply.github.com>
2025-12-24 21:10:39 +0700

    Update bili.py

--- bili.py
+++ bili.py
@@ -8,7 +8,7 @@ import json
 options = Options()
 options.add_argument("--headless")
 
-url = 'https://www.bilibili.tv/th/video/4797525106366464'
+url = 'https://www.bilibili.tv/en/video/4797920693851648'
 
 driver = webdriver.Firefox(options=options)
 driver.get(url)
